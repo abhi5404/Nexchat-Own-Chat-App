df130b07436b9f1a196c6420105b701d99f176d8
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-13 02:31:24 +0530

    feat: update HomePage layout with backdrop styling for improved UI

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import RightSidebar from '../components/RightSidebar'
 const HomePage = () => {
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'> 
-     <div>
+     <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative' >
         <Sidebar />
         <ChatContainer />
         <RightSidebar />
