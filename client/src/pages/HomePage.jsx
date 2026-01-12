d18b8d83b9366064d0c0597b19dcb1da68019240
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-13 02:26:01 +0530

    feat: restructure HomePage layout to include Sidebar, ChatContainer, and RightSidebar components

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSidebar from '../components/RightSidebar'
 
 const HomePage = () => {
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'> 
-     
+     <div>
+        <Sidebar />
+        <ChatContainer />
+        <RightSidebar />
+     </div>
+
     </div>
   )
 }
