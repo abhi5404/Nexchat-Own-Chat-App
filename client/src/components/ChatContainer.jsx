85934336458765e3e961a4e951851fdb57087884
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-17 13:58:02 +0530

    fix: correct img tag syntax and ensure proper rendering of chat messages

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -19,14 +19,13 @@ function ChatContainer({selectedUser, setSelectedUser}) {
         {messagesDummyData.map((msg, index) => (
           <div key= {index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd2838ecf9' && 'flex-row-reverse'}`}>
             {msg.image ? (
-              img src={msg.image} alt=" " className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8'/>
+              <img src={msg.image} alt=" " className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8'/>
             ) : (
-              <p></p>
+              <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd2838ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+            )}
+          </div>
+        ))}
       </div>
-
-
-
-
     </div>
   ) : (
     <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
