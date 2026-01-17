44d6aecca7feca05539924faf174e697f2f560e0
Abhijit Mondal <abhijitamp378@gmail.com>
2026-01-17 12:33:50 +0530

    feat: add user list display to Sidebar component

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { userDummyData } from '../assets/assets'
 import { useNavigate } from 'react-router-dom';
 
 const Sidebar = ({selectedUser, setSelectedUser}) => {
@@ -24,6 +24,23 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
           <input type="text" placeholder='Search User...' className='bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1' />
         </div>
       </div>
+      <div className='flex flex-col gap-4'>
+        {userDummyData.map((user, index) => (
+          <div>
+            <img src={user?.profilePic || assets.avatar_icon} alt=""
+            className='w-[35px] aspect [1/1] rounded-full' />
+            <div className='flex flex-col leading-5'>
+              <p>{user.fullName}</p>
+              {
+                index <3 
+                ? <span>Online</span>
+                 : <span>Offline</span>
+              }
+
+            </div>
+
+          }
+
     </div>
   )
 }
